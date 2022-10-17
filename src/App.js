fc78ee3788975162e6df7720317c708d6f507ab4
aivirrne <chaos.no.mai@gmail.com>
2022-10-17 23:31:57 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="weather-card">
-          <WeatherCard defaultCity="Kyiv" />
-        </div>
+        <div className="page-content">
+          <div className="weather-card">
+            <WeatherCard defaultCity="Kyiv" />
+          </div>
 
-        <div className="Footer">
-          <div className="credits">
-            <a
-              href="https://github.com/aivirrne/weather-react"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Open-source code
-            </a>{" "}
-            by Zina Mishchenko
+          <div className="Footer">
+            <div className="credits">
+              <a
+                href="https://github.com/aivirrne/weather-react"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Open-source code
+              </a>{" "}
+              by Zina Mishchenko
+            </div>
           </div>
         </div>
       </div>
